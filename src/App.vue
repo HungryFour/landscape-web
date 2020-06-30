7fc638fe2a2d42278f2af43b087473d8c8f87bc8
建明 武 <wjmwjmwb@yeah.net>
2020-06-30 18:06:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <keep-alive >
+      <router-view v-if="$route.meta.keepAlive"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive"></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+Vue.use(VueRouter);
+
+Vue.config.productionTip = false;
+
+import Index from './components/Index'
+
+const routes = [
+  {
+    path: '/home',
+    component: Index,
+    meta: {
+      keepAlive: true,
+      title: "popmart"
+    },
+  }
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
+
+router.beforeEach((to, from, next) => {
+  /* 路由发生变化修改页面title */
+  if (to.meta.title) {
+    document.title = to.meta.title;
+  }
+  if (to.fullPath == '/') {
+    next('/home');
+    return;
+  }
+  console.log("----wo zou l me");
+  next();
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
